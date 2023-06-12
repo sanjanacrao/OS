98d4bb8a113972514b8543c13fe3541ee03076b4
sanjanacrao <135594448+sanjanacrao@users.noreply.github.com>
2023-06-12 14:22:22 +0530

    Update EX2.C

--- EX2.C
+++ EX2.C
@@ -1,9 +1,15 @@
 #include<stdio.h>
-#include<sys/types.h>;
-#include<unistd.h>;
+#include<sys/types.h>
+#include<unistd.h>
+void forkexample()
+{
+    if (fork() == 0)
+    printf("Hello from child:\n");
+    else
+    printf("Hello from parent:\n");
+}
 int main()
 {
-    fork();
-    printf("hello world\n");
+    forkexample();
     return 0;
 }
